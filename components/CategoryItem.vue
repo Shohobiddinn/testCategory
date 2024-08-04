545345bac60ff2867289c5cccc271319dcce3b4b
Shohobiddinn <shohobiddinibrohimov@gmail.com>
2024-08-04 18:05:05 +0500

    finish

--- components/CategoryItem.vue
+++ components/CategoryItem.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <div @click="toggle" class="cursor-pointer py-2 flex items-center justify-between">
+    <div @click="toggle" class="cursor-pointer  hover:bg-blue-300 px-3 rounded-md py-2 flex items-center justify-between group">
       <span>{{ category.name }}</span>
-      <span v-if="category.children.length" class="transform transition-transform" :class="{ 'rotate-90': isOpen }">
+      <span v-if="category.children.length" class="transform transition-transform group-hover:translate-x-[3px] duration-100" :class="{ 'rotate-90': isOpen }">
         <i class="fa-solid fa-chevron-right"></i>
       </span>
     </div>
@@ -38,7 +38,4 @@ export default {
 </script>
 
 <style scoped>
-.cursor-pointer:hover {
-  background-color: #f0f0f0;
-}
 </style>
