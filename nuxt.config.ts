e52e3de17420e874b18eefd1585d2112456049ae
Shohobiddinn <shohobiddinibrohimov@gmail.com>
2024-08-04 17:43:32 +0500

    head title and icon create

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,13 +6,17 @@ export default defineNuxtConfig({
   css: ['./assets/css/main.css'],
   app: {
     head: {
+      title: 'Test Kategoriya',
       link: [
         {
           rel: "stylesheet",
           href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css",
-         integrity: "sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==",
+          integrity: "sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==",
           crossorigin: "anonymous",
           referrerpolicy: "no-referrer"
+        },{
+          rel:"icon",
+          href:"/images.jpg"
         }
       ]
     }
