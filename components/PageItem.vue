639b9712566a058fbf1e09ccb0793f56a45a54f5
Shohobiddinn <shohobiddinibrohimov@gmail.com>
2024-08-04 17:33:34 +0500

    components edit

--- components/PageItem.vue
+++ components/PageItem.vue
@@ -8,9 +8,6 @@
           <i class="fa-solid fa-chevron-right  hover " ></i>
         </span>
       </div>
-      <div v-if="isOpen" class="ml-4 border-l-2 pl-4">
-        <PageItem v-for="child in propsCategory?.children" :key="child.id" :category="child" />
-      </div>
     </div>
   </template>
   
@@ -34,17 +31,9 @@
         this.isOpen = !this.isOpen;
         if (item.children.length) {
           this.$emit('update', item);
-  
         }
-        // let Cat = this.props.category.children.find(cat => cat.id == item.id)
-        // this.props.category = [Cat]
-        // this.propsCategory = [item.children]
-  
       }
     },
-    components: {
-      CategoryItem: () => import('./CategoryItem.vue')
-    }
   };
   </script>
   
--- pages/category.vue
+++ pages/category.vue
@@ -5,20 +5,20 @@
              Bosh sahifa <i class="fa-solid fa-house text-[13px] ml-1"></i>
          </RouterLink>
      <div class="p-4 max-w-md  bg-white rounded-xl shadow-md space-y-4">
-       <PageItem v-for="category in categories" :key="category.id" :category="category" />
+       <CategoryItem v-for="category in categories" :key="category.id" :category="category" />
      </div>
 
   </div>
   </template>
   
   <script>
-  import PageItem from '~/components/PageItem.vue';
+  import CategoryItem from '~/components/CategoryItem.vue';
   import categoryData from '~/static/categories.json';
   
   export default {
     name: 'CategoryPage',
     components: {
-      PageItem
+      CategoryItem
     },
     data() {
       return {
